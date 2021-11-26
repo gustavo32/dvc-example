e300041a9a2957e303ec5584ca4bb83e071088fe
gustavo32 <luisouza98@gmail.com>
2021-11-26 16:26:26 -0300

    testing dvc

---
+++ pipeline.py
@@ -0,0 +1,26 @@
+from google.cloud import bigquery
+
+
+bqclient = bigquery.Client()
+
+query_string = """
+SELECT product_id, price, category, volume
+FROM `sandbox-ml-pipeline.featurestore.history`
+WHERE product_id in ('1', '2', '3')
+"""
+
+dataframe = (
+    bqclient.query(query_string)
+    .result()
+    .to_dataframe(
+        create_bqstorage_client=True
+    )
+)
+
+
+train = dataframe[:8]
+test = dataframe[8:]
+
+train.to_csv("train.csv")
+test.to_csv("test.csv")
+print(dataframe)
