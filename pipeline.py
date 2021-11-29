c916d4ce82e0655f6cfeecaab646125a9f82eb14
gustavo32 <luisouza98@gmail.com>
2021-11-29 14:44:03 -0300

    added train and test parquet dvc files

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,7 @@ bqclient = bigquery.Client()
 query_string = """
 SELECT product_id, price, category, volume
 FROM `sandbox-ml-pipeline.featurestore.history`
-WHERE product_id in ('3', '4', '5', '6', '7')
+WHERE product_id in ('4', '5', '6', '7')
 """
 
 dataframe = (
@@ -20,6 +20,6 @@ dataframe = (
 train = dataframe[:8]
 test = dataframe[8:]
 
-train.to_csv("train.csv")
-test.to_csv("test.csv")
+train.to_parquet("train.parquet")
+test.to_parquet("test.parquet")
 print(dataframe)
